1faf091b80ddca2406e0682bd37dd4de3322b339
eldjenlacasa <ger4soto@gmail.com>
2024-10-01 20:35:55 +0200

    Agregar archivos de proyecto.

---
+++ AB U20.cpp
@@ -0,0 +1,12 @@
+﻿// AB U20.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "AB U20.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello ELDJENLACA, are u oke?." << endl;
+	return 0;
+}
